ba7672e85d25e535fefb83953509b2f05e15c00d
Sundara <catch.source@gmail.com>
2018-07-25 16:38:20 +0530

    A url Get example in controller and passing to view

--- src/main/java/com/javainuse/TestController.java
+++ src/main/java/com/javainuse/TestController.java
@@ -4,12 +4,53 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.io.*;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.net.URISyntaxException;
+import java.net.URLEncoder;
+
+class GetExample {
+  OkHttpClient client = new OkHttpClient();
+
+  String run(String url) throws IOException {
+    Request request = new Request.Builder()
+        .url(url)
+        .build();
+
+    try (Response response = client.newCall(request).execute()) {
+      return response.body().string();
+    }
+  }
+}
+
 @Controller
 public class TestController {
 
+	public String siteResponse () throws IOException {
+	    GetExample example = new GetExample();
+	    String response = example.run("https://raw.github.com/square/okhttp/master/README.md");
+	    System.out.println(response);
+
+	    return response;
+	}
+
 	@RequestMapping("/welcome.html")
 	public ModelAndView firstPage() {
-		return new ModelAndView("welcome");
+		ModelAndView modelAndView = new ModelAndView("welcome");
+	    modelAndView.addObject("message", "Wonderful Life!!!");
+	    
+	    try {
+	    	String siteInfo = siteResponse();
+			modelAndView.addObject("site", URLEncoder.encode(siteInfo, "UTF-8"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return modelAndView;
 	}
+
 
 }
--- src/main/webapp/WEB-INF/jsp/welcome.jsp
+++ src/main/webapp/WEB-INF/jsp/welcome.jsp
@@ -1,1 +1,11 @@
-<h1>Welcome to Spring Boot</h1>
+<!DOCTYPE HTML>
+<html xmlns:th="http://www.thymeleaf.org">
+<head>
+    <title>SpringDemo</title>
+</head>
+<body>
+	<h1>Welcome to Spring Boot</h1>
+	<h2>${message}</h2>
+	${site}
+</body>
+</html>
